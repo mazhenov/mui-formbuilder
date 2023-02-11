4eb0416a6545647e4c9cdf5d06066309c4ad0d80
Aibek Mazhenov <aibek.mazhenov@gmail.com>
2023-02-12 05:54:54 +0600

    Start working with jest & add rollup plugins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,13 +2,22 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import terser from "@rollup/plugin-terser";
 
 import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
     input: "src/index.ts",
-    external: ["react", "react-dom", "@emotion/react", "@emotion/styled", "@mui/material", "@mui/icons-material"],
+    external: [
+      "react",
+      "react-dom",
+      "@emotion/react",
+      "@emotion/styled",
+      "@mui/material",
+      "@mui/icons-material",
+    ],
     output: [
       {
         file: packageJson.main,
@@ -22,9 +31,11 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
     ],
   },
   {
